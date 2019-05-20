739498ac37d4065b5c9568fba189117b417b0f14
Donald <donald@Donalds-PC.local>
2019-05-20 21:49:15 +0800

    增加头结点方法

--- algorithms/21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ algorithms/21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -29,12 +29,15 @@ struct ListNode
 *     ListNode(int x) : val(x), next(NULL) {}
 * };
 */
-class Solution {
+class Solution
+{
 public:
-	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
 	{
-		if (l1 == NULL) return l2;
-		if (l2 == NULL) return l1;
+		if (l1 == NULL)
+			return l2;
+		if (l2 == NULL)
+			return l1;
 
 		ListNode *head, *p;
 		head = p = NULL;
@@ -112,39 +115,78 @@ public:
 		return head;
 	}
 
-	ListNode *mergeTwoLists_recursion(ListNode *l1, ListNode *l2)
+	ListNode *mergeTwoLists_head(ListNode *l1, ListNode *l2)
 	{
-		if (l1 == NULL)
-			return l2;
-		if (l2 == NULL)
-			return l1;
+		if (l1 == NULL) return l2;
+		if (l2 == NULL) return l1;
 
-		if (l1->val <= l2->val)
+		ListNode head(0);
+		ListNode *p = &head;
+		while (l1 != NULL && l2 != NULL)
 		{
-			l1->next = mergeTwoLists_recursion(l1->next, l2);
-			return l1;
-		}
-		else
-		{
-			l2->next = mergeTwoLists_recursion(l1, l2->next);
-			return l2;
-		}
+			if (l1->val <= l2->val)
+			{
+				p->next = l1;
+				l1 = l1->next;
+			}
+			else
+			{
+				p->next = l2;
+				l2 = l2->next;
+			}
+			p = p->next;
+
+		} // end while
+
+		if (l1 != NULL)
+			p->next = l1;
+		if (l2 != NULL)
+			p->next = l2;
+		
+		return head.next;
 	}
-};
 
-void trimLeftTrailingSpaces(string &input) {
+}
+
+ListNode *mergeTwoLists_recursion(ListNode *l1, ListNode *l2)
+{
+	if (l1 == NULL)
+		return l2;
+	if (l2 == NULL)
+		return l1;
+
+	if (l1->val <= l2->val)
+	{
+		l1->next = mergeTwoLists_recursion(l1->next, l2);
+		return l1;
+	}
+	else
+	{
+		l2->next = mergeTwoLists_recursion(l1, l2->next);
+		return l2;
+	}
+}
+}
+;
+
+void trimLeftTrailingSpaces(string &input)
+{
 	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-		return !isspace(ch);
-	}));
+					return !isspace(ch);
+				}));
 }
 
-void trimRightTrailingSpaces(string &input) {
+void trimRightTrailingSpaces(string &input)
+{
 	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-		return !isspace(ch);
-	}).base(), input.end());
+					return !isspace(ch);
+				})
+					.base(),
+				input.end());
 }
 
-vector<int> stringToIntegerVector(string input) {
+vector<int> stringToIntegerVector(string input)
+{
 	vector<int> output;
 	trimLeftTrailingSpaces(input);
 	trimRightTrailingSpaces(input);
@@ -153,20 +195,23 @@ vector<int> stringToIntegerVector(string input) {
 	ss.str(input);
 	string item;
 	char delim = ',';
-	while (getline(ss, item, delim)) {
+	while (getline(ss, item, delim))
+	{
 		output.push_back(stoi(item));
 	}
 	return output;
 }
 
-ListNode* stringToListNode(string input) {
+ListNode *stringToListNode(string input)
+{
 	// Generate list from the input
 	vector<int> list = stringToIntegerVector(input);
 
 	// Now convert that list into linked list
-	ListNode* dummyRoot = new ListNode(0);
-	ListNode* ptr = dummyRoot;
-	for (int item : list) {
+	ListNode *dummyRoot = new ListNode(0);
+	ListNode *ptr = dummyRoot;
+	for (int item : list)
+	{
 		ptr->next = new ListNode(item);
 		ptr = ptr->next;
 	}
@@ -175,27 +220,32 @@ ListNode* stringToListNode(string input) {
 	return ptr;
 }
 
-string listNodeToString(ListNode* node) {
-	if (node == nullptr) {
+string listNodeToString(ListNode *node)
+{
+	if (node == nullptr)
+	{
 		return "[]";
 	}
 
 	string result;
-	while (node) {
+	while (node)
+	{
 		result += to_string(node->val) + ", ";
 		node = node->next;
 	}
 	return "[" + result.substr(0, result.length() - 2) + "]";
 }
 
-int main() {
+int main()
+{
 	string line;
-	while (getline(cin, line)) {
-		ListNode* l1 = stringToListNode(line);
+	while (getline(cin, line))
+	{
+		ListNode *l1 = stringToListNode(line);
 		getline(cin, line);
-		ListNode* l2 = stringToListNode(line);
+		ListNode *l2 = stringToListNode(line);
 
-		ListNode* ret = Solution().mergeTwoLists_recursion(l1, l2);
+		ListNode *ret = Solution().mergeTwoLists_recursion(l1, l2);
 
 		string out = listNodeToString(ret);
 		cout << out << endl;
